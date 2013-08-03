fd1c6ebb324cdf5039cefe34d7cc0cd72943a573
Sahan H <sahan@sahanz.com>
2013-08-03 23:24:53 +0530

    added testing for mutators

--- tests/Entity/ModelTest.php
+++ tests/Entity/ModelTest.php
@@ -13,9 +13,31 @@ class ModelTest extends PHPUnit_Framework_TestCase
 		$this->assertAttributeEquals(array('id' => 2, 'first_name' => 'Sahan', 'last_name' => 'H'), 'attributes', $model);
 	}
 
-	public function testSetAttributeWithMutator()
+	public function testFillRaw()
 	{
-		
+		$data  = array('id' => 2, 'first_name' => 'Sahan', 'last_name' => 'H');
+		$model = new SampleModel;
+		$model->fillRaw($data);
+
+		$this->assertAttributeEquals($data, 'attributes', $model);
+	}
+
+	public function testFill()
+	{
+		$data  = array('id' => 2, 'first_name' => 'Sahan', 'last_name' => 'H');
+		$model = new SampleModel;
+		$model->fillRaw($data);
+
+		$this->assertAttributeEquals($data, 'attributes', $model);
+	}
+
+	public function testGetSetAttributeWithMutator()
+	{
+		$model = new SampleModelConfigured;
+		$model->first_name = 'SAHAN';
+
+		$this->assertAttributeEquals(array('first_name' => 'sahan'), 'attributes', $model);
+		$this->assertEquals('SAHAN', $model->first_name);
 	}
 
 	public function testEntityPath()
