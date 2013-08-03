7c5796ad6bb41a18b1aff14f9588f6a6de268c48
Sahan H <sahan@sahanz.com>
2013-08-04 00:36:10 +0530

    model collection class init

--- src/Resto/Entity/Collection.php
+++ src/Resto/Entity/Collection.php
@@ -6,5 +6,20 @@ namespace Resto\Entity;
 
 class Collection
 {
+	protected $items;
 
+	public function __construct($items, $meta = false)
+	{
+		$this->items = $items;
+	}
+
+	/**
+	 * Get the first item from the collection.
+	 *
+	 * @return mixed|null
+	 */
+	public function first()
+	{
+		return count($this->items) > 0 ? reset($this->items) : null;
+	}
 }
