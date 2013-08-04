2a176722570016a15553d50ec3070f71903b3468
Sahan H <sahan@sahanz.com>
2013-08-04 09:33:11 +0530

    meta field support

--- src/Resto/Entity/Collection.php
+++ src/Resto/Entity/Collection.php
@@ -4,13 +4,31 @@
  */
 namespace Resto\Entity;
 
+use Resto\Common\Helpers as H;
+
 class Collection
 {
 	protected $items;
 
+	protected $meta = array();
+
 	public function __construct($items, $meta = false)
 	{
 		$this->items = $items;
+		$this->meta  = $meta;
+	}
+
+	/**
+	 * Get meta data, key support dot notation levels
+	 * @param  string $key
+	 * @return mixed
+	 */
+	public function getMeta($key = null)
+	{
+		if (!is_array($this->meta))
+			return $this->meta;
+		else
+			return H::arrayGet($this->meta, $key);	
 	}
 
 	/**
