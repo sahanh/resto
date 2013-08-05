6f6d2c9346fcd706e89838abd17a13303448d91a
Sahan H <sahan@sahanz.com>
2013-08-05 14:05:26 +0530

    refactored and introduced http method based data formatting for requests

--- src/Resto/Parser/Request/DefaultParser.php
+++ src/Resto/Parser/Request/DefaultParser.php
@@ -13,15 +13,77 @@ class DefaultParser implements ParserInterface
 
 	public function __construct(Query $query)
 	{
-		$this->query = $query;
+		$this->query   = $query;
+		$this->request = $query->getRequest(); 
+	}
+
+	/**
+	 * Set query params as query string
+	 * Parser will call this method on GET requests
+	 */
+	protected function setGetData()
+	{
+		$this->request->addParams($this->query->getParams());
+	}
+
+	/**
+	 * Set query params as form fields of the request
+	 * Parser will call this method on POST requests
+	 */
+	protected function setPostData()
+	{
+		$this->request->addPostFields($this->query->getParams());
+	}
+
+	/**
+	 * Parser will call this on DELETE requests
+	 */
+	protected function setDeleteData()
+	{
+
+	}
+
+	/**
+	 * Set query params as form fields of the request
+	 * Parser will call this method on PUT requests
+	 */
+	protected function setPutData()
+	{
+		$this->request->addPostFields($this->query->getParams());
+	}
+
+	protected function callDataFormatter($http_method)
+	{
+		$http_method = ucfirst(strtolower($http_method));
+		$method      = "set{$http_method}Data";
+		
+		if (method_exists($this, $method)) {
+			$this->{$method}();
+		}
 	}
 
 	public function getRequest()
 	{
-		$query   = $this->query;
-		$request = $query->getRequest();
-		$request->setPath($query->getPath());
-		$request->addParams($query->getParams());
-		return $request;
+		switch ($this->request->getMethod()) {
+
+			case 'POST':
+					$this->setPostData();
+				break;
+
+			case 'PUT':
+					$this->setPutData();
+				break;
+
+			case 'DELETE':
+					$this->setDeleteData();
+				break;
+					
+			default:
+					$this->setGetData();
+				break;
+		}
+
+		$this->request->setPath($this->query->getPath());
+		return $this->request;
 	}
 }
